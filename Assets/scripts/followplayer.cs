f53d7b9d88d2a439e7eec63dff7285159e21cbeb
haelie229 <56128971+haelie229@users.noreply.github.com>
2019-11-07 15:49:58 -0800

    i added more codes

---
+++ Assets/scripts/followplayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followplayer : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = player.transform.position - transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position - offset;
+
+    }
+}
--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {
+    GameObject player;
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,8 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {// Move the vehicle forard
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position;
 
 
         
