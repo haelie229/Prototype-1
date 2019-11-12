5fd75fa77543c0b79a5918fc16db5c11bac8324a
haelie229 <56128971+haelie229@users.noreply.github.com>
2019-11-12 09:22:58 -0800

    moved stuff arond

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playercontroller : MonoBehaviour
-{
+{ 
     GameObject player;
     public float speed = 5.0f;
     // Start is called before the first frame update
@@ -16,7 +16,7 @@ public class playercontroller : MonoBehaviour
     void Update()
     {// Move the vehicle forard
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.position = player.transform.position;
+        
 
 
         
